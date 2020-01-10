2fc19163385242805fe12c733c9da99b964e66bc
Alan Turing <austeyd@gmail.com>
2020-01-10 13:50:02 -0700

    Update rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { namesReducer } from './namesReducer';
 import { typesReducer } from './typesReducer';
 import { errorReducer } from './errorReducer';
 import { loadingReducer } from './loadingReducer';
+import { pokemonReducer } from './pokemonReducer';
 
 const rootReducer = combineReducers({
   pokeNames: namesReducer,
   pokeTypes: typesReducer,
   errorMessage: errorReducer,
-  loadingStatus: loadingReducer
+  loadingStatus: loadingReducer,
+  pokeData: pokemonReducer
 });
 
 export default rootReducer;
