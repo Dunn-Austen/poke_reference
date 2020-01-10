704fba2550f447a31a93d90872b5ca2b76625006
Alan Turing <austeyd@gmail.com>
2020-01-09 21:01:40 -0700

    Establish scaffolding for intial fetch

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
+import { fetchEmAll } from '../../apiCalls'
+import { getPokedata } from '../../actions'
 
 export class App extends Component {
+  componentDidMount() {
+    const { getPokedata } = this.props
+    fetchEmAll()
+      .then(data => {
+
+      })
+      .catch(error => {
+        this.props.handleError('Error with pokedata retrieval')
+      })
+  }
 
   render = () => {
     return (
