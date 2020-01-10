ff58cd5ae02661dfd18e8450c5f49aec2130eab6
Alan Turing <austeyd@gmail.com>
2020-01-10 13:50:14 -0700

    Connect SearchBar to the store

--- src/containers/SearchBar/SearchBar.js
+++ src/containers/SearchBar/SearchBar.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './SearchBar.css';
-// import { handleError } from '../../actions/index';
-import { cacheTypes } from '../../apiCalls';
+import { fetchPokemonData, fetchTypes } from '../../apiCalls'
+import { storePokemon, cacheTypes, handleError, isLoading } from '../../actions';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-export const SearchBar = props => {
 
+export const SearchBar = props => {
+  const { storePokemon, cacheTypes, handleError, isLoading } = props;
+  let inputValue;
+  
   return (
     <section className='search-container'>
       <h1 className='search-prompt'>Find your Foe</h1>
-      <label htmlFor='pokefoe-input'>Search PokeNemesis</label>
+      <label htmlFor='pokefoe-input'>Pokemon Name</label>
       <input
         id='pokefoe-input'
         type='text'
-        placeholder='Email Address'
+        placeholder='Mind your spelling'
+        onChange={(event) => {
+          inputValue = event.target.value
+        }}
       />
-      <button className="search-btn">
+      <button
+        className="search-btn"
+        onClick={(event) => {
+          storePokemon(inputValue)
+        }}
+      >
         Search
       </button>
     </section>
   )
 }
 
-export default SearchBar;
+export const mapDispatchToProps = dispatch => ({
+  storePokemon: pokeData => dispatch(storePokemon(pokeData)),
+  cacheTypes: pokeTypes => dispatch(cacheTypes(pokeTypes)),
+  handleError: errorMessage => dispatch(handleError(errorMessage))
+});
+
+export default connect(null, mapDispatchToProps)(SearchBar);
+
+SearchBar.propTypes = {
+  storePokemon:PropTypes.func,
+  cacheTypes: PropTypes.func,
+  handleError: PropTypes.func,
+}
