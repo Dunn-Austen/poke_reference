1a4caa6a1da4e016ebcc69b5c1123ccc6794d0c3
Alan Turing <austeyd@gmail.com>
2020-01-09 21:50:57 -0700

    Add mapStateToProps test for the App component

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { shallow } from 'enzyme';
+import { App, mapStateToProps, mapDispatchToProps } from './App';
+import { cachePokedata, handleError, isLoading } from '../../actions';
+import { fetchEmAll } from './../../apiCalls';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock('./../../apiCalls.js');
+
+describe('App', () => {
+  let wrapper, mockAddMovies, mockHandleError, mockIsLoading, mockMoviesData;
+
+  describe('mapsStateToProps', () => {
+    it('should return only the pertinent information from the redux store', () => {
+      const mockState = {
+        pokeData: [{pokemon: 'name', url: 'url'}],
+        errorMessage: '',
+        isLoading: false
+      };
+      const expected = {
+        pokeData: [{pokemon: 'name', url: 'url'}],
+      };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+})
