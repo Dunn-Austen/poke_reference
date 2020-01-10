507fd0cfcd737a625b860c565ca13af5f2bd44cb
Alan Turing <austeyd@gmail.com>
2020-01-09 17:04:39 -0700

    Configure Router

---
+++ src/containers/App/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+import { Route } from 'react-router-dom';
+
+export class App extends Component {
+
+  render = () => {
+    return (
+      <main>
+        Placeholder
+      </main>
+    );
+  };
+}
+
+export default App;
