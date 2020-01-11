466a7db38d83b089fcad772e6aed06572be48c03
Alan Turing <austeyd@gmail.com>
2020-01-11 16:06:29 -0700

    Begin to style PokePage

--- src/containers/NavBar/NavBar.js
+++ src/containers/NavBar/NavBar.js
@@ -15,7 +15,7 @@ export const NavBar = props => {
         <h1>Welcome (PokeReference)</h1>
       }/>
       <Route exact path='/pokepage' render={() =>
-        <h1>{pokeData.name.toUpperCase()}'s Page</h1>
+        <h1>{pokeData.name}'s Page</h1>
       }/>
     </header>
   )
