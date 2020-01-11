84586c125711ea0d0a2ceb699e1b83cd6dbd47c0
Alan Turing <austeyd@gmail.com>
2020-01-11 14:10:56 -0700

    Build initial JSX elements for PokePage

---
+++ src/containers/PokePage/PokePage.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './PokePage.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+
+export const PokePage = props => {
+  let inputValue;
+  const { pokeData, pokeTypes } = props;
+
+  return (
+    <section className='poke-page'>
+      <header>{pokeData.name.toUpperCase()}</header>
+      <div className='sections-container'>
+        <section className='main-left'>
+        </section>
+        <section className='main-right'>
+
+        </section>
+    </section>
+  )
+}
+
+export const mapStateToProps = state => ({
+  pokeData: state.pokeData,
+  pokeTypes: state.pokeTypes
+})
+
+export default connect(mapStateToProps, null)(PokePage);
+
+PokePage.propTypes = {
+  pokeData: PropTypes.object,
+  pokeTypes: PropTypes.array
